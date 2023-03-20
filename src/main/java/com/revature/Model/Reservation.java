c328e00857cbc8b6748c7b1b1f6b73bb29284da7
alex <ackerander@gmail.com>
2023-03-20 14:24:03 -0700

    Reservation model implementation.

---
+++ src/main/java/com/revature/Model/Reservation.java
@@ -0,0 +1,37 @@
+package com.revature.Model;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+@Entity
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	private Date reservation_time;
+
+	@Column
+	private int seats;
+
+	@Column
+	private String special_accommodations;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JsonBackReference
+	@JoinColumn(name = "restaurant_id")
+	private Restaurant restaurant;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JsonBackReference
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+}
