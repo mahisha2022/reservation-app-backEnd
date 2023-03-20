27285c3b76f1fab89e25693f821f02d41998a61a
alex <ackerander@gmail.com>
2023-03-20 14:50:01 -0700

    Reservation service implementation.

--- src/main/java/com/revature/Repository/ReservationRepository.java
+++ src/main/java/com/revature/Repository/ReservationRepository.java
@@ -2,8 +2,9 @@ package com.revature.Repository;
 
 import com.revature.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-	List<Reservation> findAllByCustomer(Customer customer);
-	List<Reservation> findAllByRestaurant(Restaurant restaurant);
+	List<Reservation> findAllByRestaurant(long restaurant_id);
+	List<Reservation> findAllByCustomer(long customer_id);
 }
---
+++ src/main/java/com/revature/Service/ReservationService.java
@@ -0,0 +1,39 @@
+package com.revature.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.Model.*;
+import com.revature.Repository.*;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ReservationService {
+	ReservationRepository reservationRepository;
+	RestaurantRepository restaurantRepository;
+	CustomerRepository customerRepository;
+
+	@Autowired
+	public ReservationService(ReservationRepository reservationRepository, RestaurantRepository restaurantRepository, CustomerRepository customerRepository) {
+		this.reservationRepository = reservationRepository;
+		this.restaurantRepository = restaurantRepository;
+		this.customerRepository = customerRepository;
+	}
+
+	public Reservation registerReservation(long restaurant_id, long customer_id, Reservation reservation) {
+		Restaurant r = restaurantRepository.findById(restaurant_id).get();
+		Customer c = customerRepository.findById(customer_id).get();
+		r.getReservations().add(reservation);
+		c.getReservations().add(reservation);
+		return reservationRepository.save(reservation);
+	}
+
+	public List<Reservation> getAllReservationsByRestaurant(long restaurant_id) {
+		return reservationRepository.findAllByRestaurant(restaurant_id);
+	}
+
+	public List<Reservation> getAllReservationsByCustomer(long customer_id) {
+		return reservationRepository.findAllByCustomer(customer_id);
+	}
+}
