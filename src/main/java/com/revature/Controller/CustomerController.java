87535254e12b8a48f933ae01b68293e26e849d9d
alex <ackerander@gmail.com>
2023-03-30 12:05:57 -0700

    Resolved CORS issue

--- src/main/java/com/revature/Controller/CustomerController.java
+++ src/main/java/com/revature/Controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.revature.Model.*;
 import com.revature.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
 public class CustomerController {
 	CustomerService customerService;
 
